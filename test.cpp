fc23693837f085d006e8c775ae43612dbc6943d2
Lucas Amaro <16543218+lcsamaro@users.noreply.github.com>
2021-07-15 23:22:06 -0300

    More parser tests

--- test.cpp
+++ test.cpp
@@ -4,6 +4,7 @@
 #include <functional>
 #include <utility>
 #include <vector>
+#include <iostream>
 
 #include "parser.h"
 
@@ -32,7 +33,46 @@ std::vector<result> parse_string(std::string s) {
 	return v;
 }
 
-TEST_CASE( "Parser works correctly", "[parser]" ) {
-	REQUIRE( parse_string(" 20:37 InitGame: abc").size() == 1 );
+TEST_CASE( "Parser ignores hyphenetade lines", "[parser]" ) {
+	auto r = parse_string(" 20:37 ----------------");
+	REQUIRE( r.size() == 0 );
+}
+
+TEST_CASE( "Parser ignores malformed lines", "[parser]" ) {
+	auto r = parse_string("hello world\n\r\r\n\n");
+	REQUIRE( r.size() == 0 );
+}
+
+TEST_CASE( "Parser works correctly for InitGame", "[parser]" ) {
+	auto r = parse_string(
+		" 20:37 InitGame: abc\n"
+		" 20:37 InitGame: abc");
+	REQUIRE( r.size() == 2 );
+	REQUIRE( r.front().event == "InitGame" );
+	REQUIRE( r.front().text == "" ); // should be empty
+	REQUIRE( r.back().event == "InitGame" );
+	REQUIRE( r.back().text == "" ); // should be empty
+}
+
+TEST_CASE( "Parser works correctly for ShutdownGame", "[parser]" ) {
+	auto r = parse_string(
+		" 20:37 ShutdownGame: abc\n"
+		" 20:37 ShutdownGame: abc");
+	REQUIRE( r.size() == 2 );
+	REQUIRE( r.front().event == "ShutdownGame" );
+	REQUIRE( r.front().text == "" ); // should be empty
+	REQUIRE( r.back().event == "ShutdownGame" );
+	REQUIRE( r.back().text == "" ); // should be empty
+}
+
+TEST_CASE( "Parser works correctly for Kill", "[parser]" ) {
+	auto r = parse_string(
+		" 20:37 Kill: 1022 2 22: aaa\n"
+		" 20:37 Kill: 1 1 1: bbb\n");
+	REQUIRE( r.size() == 2 );
+	REQUIRE( r.front().event == "Kill" );
+	REQUIRE( r.front().text == "aaa" );
+	REQUIRE( r.back().event == "Kill" );
+	REQUIRE( r.back().text == "bbb" );
 }
 
